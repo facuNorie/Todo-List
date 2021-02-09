fcc8cc899b0aab24e9a929cda3e33e95d7f9836f
facuNorie <noriegafacundo08@gmail.com>
2021-02-09 18:20:33 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Prevent Default of Input Text
+document.querySelector(".form__text").addEventListener("keypress", e =>{
+    if(e.keyCode == 13) e.preventDefault();
+});
+
+// Calling Button
+let btn = document.querySelector(".form__btn");
+btn.addEventListener("click",()=>{
+    generateItem(document.querySelector(".form__text").value);
+    
+});
+// Generation Item List
+const generateItem = (item)=>{
+    if(item == "") return ;
+    // Create item
+    let div = document.createElement("DIV");
+    let p = document.createElement("P");
+    let button = document.createElement("BUTTON");
+    // Add Class
+    div.classList.add("list__div");
+    p.classList.add("list__item");
+    button.classList.add("list__btn");
+    // Content of P and Button
+    p.textContent = item;
+    button.innerHTML = "X";
+    // Child
+    div.appendChild(p);
+    div.appendChild(button);
+    document.querySelector(".list").appendChild(div);
+    // Clean Input
+    document.querySelector(".form__text").value = "";
+    // DELETE BTN 
+    button.addEventListener("click",()=>{
+        document.querySelector(".list").removeChild(div);
+    });
+}
+
+// AÑADIR "2DA OPORTUNIDAD EN DELETE BTN"
